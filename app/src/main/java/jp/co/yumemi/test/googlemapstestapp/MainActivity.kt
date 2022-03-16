e02c1b50d726b0131f1c0596cd5e8bda35b14d7b
Go Shibata <g_shibata@yumemi.co.jp>
2022-03-16 14:04:37 +0900

    initial commit

---
+++ app/src/main/java/jp/co/yumemi/test/googlemapstestapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package jp.co.yumemi.test.googlemapstestapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import jp.co.yumemi.test.googlemapstestapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+    }
+}
